7084e5fbc068c1169875093e3b6808e2e4f2d0d1
Halik <michaelodumosu57@gmail.com>
2019-01-26 01:33:51 -0500

    learned that Atlas is not required and properly setup my connection string

--- connection.js
+++ connection.js
@@ -1,9 +1,10 @@
 var MongoClient = require('mongodb').MongoClient;
-var connection_string = require('./credentials.js').c_string
-
-var uri = connection_string;
-MongoClient.connect(uri,  { useNewUrlParser: true }  function(err, db) {
+var c_string = require('./impt.js').c_string
+const circular_replacer = require('./circular_replacer.js')
+var uri = c_string;
+MongoClient.connect(uri,  { useNewUrlParser: true } , function(err, db) {
   // Paste the following examples here
-
+  console.log(err)
+  console.log(JSON.stringify(db,circular_replacer()))
   db.close();
 });
