64a73f8d0f5bc9174af98188796046f9afab31ad
Halik <michaelodumosu57@gmail.com>
2019-01-26 14:46:20 -0500

    made a proper template on how to properly open a connection with MongoDB and handle any errors that may arise

--- MongoDB_Node/connection.js
+++ MongoDB_Node/connection.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const events = require('events')
 const path = require('path');
 // const assert = require('assert')
-const required_dir = path.join(process.env.HOME, 'My_Computer/NVM/NODE/MINI_PROJECTS/PIPE_STREAM')
+const required_dir = path.join(process.env.HOME, 'req_mod_node')
 const read_monitor = require(required_dir + '/read_monitor.js')
 const async_listener = require(required_dir +'/async_listener.js')
 const node_mode = require(required_dir +'/node_mode.js')
@@ -19,27 +19,29 @@ const d_rn = drain()
 const s_f = stream_finished()
 const a_l = async_listener()
 const p_l = pipeline()
-const p_uE = require(   required_dir + 'process_uncaughtException.js'   )
+const p_uE = require(   required_dir + '/process_uncaughtException.js'   )
 p_uE.message = 'closed the MongoDB instance I believe'
 p_uE.MongoClient_instance = function(   dev_obj   ){
 	dev_obj.c_Mongo.close()
 }
+
 var MongoClient = require('mongodb').MongoClient;
 var MongoDB_data = require('./MongoDB_data.js')
 var url = 'mongodb://localhost:27017';
-MongoDB_data.client = new MongoClient(   url   ) 
+MongoDB_data.client = new MongoClient(   url,{ useNewUrlParser: true }   ) 
 const client = MongoDB_data.client
-const close_MongoDB = a_l(function(   dev_obj   ){
+const close_MongoDB = function(   dev_obj   ){
+	console.log(   JSON.stringify(   dev_obj,circular_replacer(),2   )   )
 	dev_obj.c_Mongo.close()
-})
-
+}
 p_uE.open_items.push([
 						"MongoClient_instance",
 						{
 							c_Mongo :MongoDB_data.client
 						}
 					])
-
+console.log(   p_uE   )
+debugger;
 
 
 
@@ -67,7 +69,9 @@ client.connect(function(err) {
 
 
 		console.log(   err   )
-		close_MongoDB(   c_Mongo   )
+		close_MongoDB({
+						c_Mongo:client
+					  })
 
     
 	}	
@@ -76,7 +80,10 @@ client.connect(function(err) {
 	console.log("Connected correctly to server");
 	const db = client.db(   MongoDB_data.db_1_i.name   );
 	console.log(   JSON.stringify(   db,circular_replacer(),2   )   )
-	insertDocuments(db, function() {  
-	  	close_MongoDB(   c_Mongo   )    
-	});
+	setImmediate(() =>{
+		close_MongoDB({
+						c_Mongo:MongoDB_data.client
+					  })   
+	})	
+	
 });
